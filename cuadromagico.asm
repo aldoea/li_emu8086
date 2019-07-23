c60796ae142ae0556a80e48186e89430d455d4c5
aldoea <aldo.ea@hotmail.com>
2019-07-23 11:41:39 -0500

    Guardando progreso antes de que mi computadora muera porque acaba de quemarse mi cargador

--- cuadromagico.asm
+++ cuadromagico.asm
@@ -1,19 +1,19 @@
 name "CUADRO MAGICO"
 org 100h
 
-MwriteToFile MACRO number
+MwriteToFile MACRO value
 		; write to file:
 		mov ah, 40h
 		mov bx, filehandler
-		mov varhelper, number
+		mov varhelper, value		
 		mov dx, offset varhelper
-		mov cx, 2
+		mov cx, bytesize
 		int 21h
 ENDM
 
-Mseek MACRO index
-	mov ax, index
-	mov bx, 2
+Mseek MACRO
+	mov ax, index_casilla
+	mov bx, bytesize
 	mul bx
 	mov di, ax
 
@@ -23,6 +23,15 @@ Mseek MACRO index
 	mov dx, di
 	mov ah, 42h
 	int 21h
+ENDM
+
+Mcalculate_index_casilla MACRO i, j
+	mov ax, i_index
+	mov bx, inputNum
+	mul bx
+	mov bx, j_index
+	add ax, bx
+	mov index_casilla, ax
 ENDM
 
 MopenFile MACRO
@@ -57,7 +66,7 @@ endm
 
 .data
 	input DB 31 DUP(?),"$"
-	inputNum db ?
+	inputNum dw ?
 	squaresize dw ?
 	msg db "Please type the size of the magic square :) " , 0Dh,0Ah, "$"
 	msg_badinput db "Please type only real numbres starting from 3 :) " , 0Dh,0Ah, "$"
@@ -66,6 +75,11 @@ endm
 	filename db 'squarefile.txt',0
 	filehandler dw 0
 	varhelper  dw 0
+	i_index  dw ?
+	j_index  dw ?
+	index_casilla dw ?
+	bytesize dw 2
+	contador dw 0
 	
 .code
 	START:
@@ -99,7 +113,7 @@ endm
 	  MAINPROC:
 	  	test bl, 1
 	    jz BADINPUT
-	  	mov inputNum, bl
+	  	mov inputNum, bx
 	  	mov al, bl
 	  	mul bl
 	  	mov squaresize, ax
@@ -112,24 +126,57 @@ endm
 			jc EXEPTION
 			mov filehandler, ax		
 
-			mov cx, squaresize
-			mov si, 0
-			mov di, squaresize
-			dec di
-			inc si
-			FORI:
-				push cx
-				push di
-				Mseek di
-				pop di
-				MwriteToFile si
-				pop cx
-				inc si
-				dec di
-			LOOP FORI
+
+			mov i_index, 0 ; fila
+			mov ax, inputNum
+			mov bx, 2   
+			xor dx, dx
+			div bx
+			mov j_index, ax  ; columna
+			inc j_index
+			Mcalculate_index_casilla
+			Mseek
+			mov cx, 1 ; Contador
+			mov contador, cx			
+			MwriteToFile cx
+			mov cx, contador			
 			
+			MAINWHILE: ; while(cx >= 0):
+				cmp cx, squaresize
+				jge BREAK
+				mov ax, i_index
+				dec ax
+				cmp ax, 0				
+				jnge E1
+				I1:
+					Mprint msg_ok
+				E1:
+					mov ax, j_index
+					inc ax
+					cmp ax, inputNum
+					jnl E1_E1
+					E1_I1:
+						Mprint msg_ok
+					E1_E1:
+						inc i_index
+						inc contador
+						mov cx, contador
+						Mcalculate_index_casilla
+						Mseek
+						MwriteToFile cx
+						mov cx, contador
+					jmp MAINWHILE
 
 
+
+				;mov
+				; Mseek di				
+				; MwriteToFile si
+				; pop cx
+				; inc si
+				; dec di
+			;LOOP FORI
+			BREAK:			
 			McloseFile
 
 			Mprint msg_ok
